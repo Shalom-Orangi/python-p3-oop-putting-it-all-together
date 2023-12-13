dadae4df63f87a76f4579b6367ac68006d9db4a5
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2023-12-13 22:56:17 +0300

    Update

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self,title,page_count):
+        self.title=title
+        self.page_count=(page_count)
+
+    def turn_page(self):
+        if isinstance(self.page_count,int):
+            print("Flipping the page...wow, you read fast!")
+        else:
+            print("page_count must be an integer")
+
+book = Book("The World According to Garp", 69)
+book.turn_page()
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,size):
+        self.brand=brand
+        self.size=size
+        self.condition="New"
+
+    def check_size(self):
+        if not isinstance(self.size,int):
+            print("size must be an integer")  
+
+    def repair (self):
+        print("The shoe has been repaired")
+        self.condition="New"
+
+shoe= Shoe("Adidas",9)
+shoe.check_size()
